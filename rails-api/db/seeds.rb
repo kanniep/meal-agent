06ae42329f95f01e8c52cb041fb90e98967a92ba
Kantapon Pornprasertsakul <st120367@ait.asia>
2018-10-19 13:35:52 +0700

    Add rails_admin page

--- rails-api/db/seeds.rb
+++ rails-api/db/seeds.rb
@@ -1,5 +1,5 @@
-admin_role = Role.create(name: 'admin')
-shop_owner_role = Role.create(name: 'shop_owner')
-ordinary_role = Role.create(name: 'ordinary')
+admin_role = Role.create(id: 1, name: 'admin')
+shop_owner_role = Role.create(id: 2, name: 'shop_owner')
+ordinary_role = Role.create(id: 3, name: 'ordinary')
 
-admin = User.create(email: 'st120367@ait.asia', password: 'Kan21341722', roles: [admin_role, ordinary_role])
+admin = User.create(id: 1, email: 'st120367@ait.asia', password: 'Kan21341722', roles: [admin_role, ordinary_role])
